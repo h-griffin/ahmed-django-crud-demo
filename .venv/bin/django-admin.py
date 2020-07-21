9bed17bc102c8c01a47c160f887284ae87cf099a
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-07-20 19:11:42 -0700

    first commit

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/haleygriffin/codefellows/401-python/ahmed-django-crud-demo/.venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
